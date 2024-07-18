29046d30d9ce483f91f4b3f429a5b876f9e8ef85
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-18 13:03:12 +0530

    Create 16-7.java

---
+++ 16-7.java
@@ -0,0 +1,20 @@
+/* 16-07-2024 */
+/*this is java*/
+/*it is going
+to work*/
+public class Test
+{
+public static void main(String[] args)
+{
+	System.out.println("To infinity and beyond");
+	String name="John";
+	System.out.println(name);
+	int myNum = 15;
+	System.out.println(myNum);
+	myNum=20;
+	System.out.println(myNum);
+	final int Num=15;
+	System.out.println(Num);
+	Num=29; /* error thrown while compiling the file */ /* error: cannot assign a value to final variable Num */
+}
+}
