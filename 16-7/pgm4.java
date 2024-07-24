7b730fa845337a45d498f992ddbfbff977df3b62
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 05:16:03 +0530

    Create pgm4.java

---
+++ 16-7/pgm4.java
@@ -0,0 +1,12 @@
+/* 16-07-2024 */
+/*23BAI1354*/
+public class Pgm4
+{
+public static void main(String[] args)
+{
+	int myNum = 15;
+	System.out.println(myNum);
+    myNum=20;
+	System.out.println(myNum);
+}
+}
