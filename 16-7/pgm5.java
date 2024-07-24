6c64862891c7ca4b7feb3621e8a8b35aa48e4f73
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 05:17:47 +0530

    Create pgm5.java

---
+++ 16-7/pgm5.java
@@ -0,0 +1,11 @@
+/* 16-07-2024 */
+/*23BAI1354*/
+public class Pgm5
+{
+public static void main(String[] args)
+{
+	final int Num=15;
+	System.out.println(Num);
+	Num=29; /* error thrown while compiling the file */ /* error: cannot assign a value to final variable Num */
+}
+}
