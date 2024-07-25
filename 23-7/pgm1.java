5e39cd89b7503d1bbeb17297aa4314fe1d5f039c
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 06:53:14 +0530

    Create pgm1.java

---
+++ 23-7/pgm1.java
@@ -0,0 +1,7 @@
+class pgm1 {
+    public static void main(String[] args) {
+        boolean isJavafun = true;
+        boolean isFishtasty = false;
+        System.out.println(isJavafun);
+        System.out.println(isFishtasty);
+    }
