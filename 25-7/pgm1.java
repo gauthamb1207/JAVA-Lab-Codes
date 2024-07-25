4c9c2f17c375a1f9e67b569e4c787b52fd7a2d47
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 12:29:55 +0530

    Create pgm1.java

---
+++ 25-7/pgm1.java
@@ -0,0 +1,8 @@
+class Pgm1{
+    public static void main(String[] args)
+    {
+        char[] helloArray={'h','e','l','l','o'};
+        String HelloString = new String(helloArray);
+        System.out.println(HelloString);
+    }
+}
