2f9fb1cac95fd5f31766be4f74cc8adb1f2e402d
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 05:37:09 +0530

    Create pgm1.java

---
+++ 22-7/pgm1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Pgm1
+{
+public static void main(String[] args)
+{
+    
+    String firstName= "John";
+    String lastName="Doe";
+    System.out.println(firstName.concat(lastName));
+}
+}
