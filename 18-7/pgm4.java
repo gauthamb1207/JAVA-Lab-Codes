a2da1f4133f1e6e83f6752a46d20791fff881fa0
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 05:30:19 +0530

    Create pgm4.java

---
+++ 18-7/pgm4.java
@@ -0,0 +1,8 @@
+public class Pgm4
+{
+public static void main(String[] args)
+{
+	String txt="abcdefghijklmnopqrstuvwxyz";
+	System.out.println("The length og the txt string is: "+txt.length());
+}
+}
