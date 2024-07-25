581665a9d757a8a6e24149787dcb9093131cbce9
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 05:40:11 +0530

    Create pgm8.java

---
+++ 22-7/pgm8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Pgm8
+{
+public static void main(String[] args)
+{
+    
+    System.out.println(Math.random()); /* only from 0.0 (inc) to 1.0 (exc) and cannot be specified */
+}
+}
