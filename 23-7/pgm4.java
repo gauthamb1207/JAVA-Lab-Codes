e89bb1e420b88a79ebdbdb14ff843a7534d4c567
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-25 07:04:32 +0530

    Create pgm4.java

---
+++ 23-7/pgm4.java
@@ -0,0 +1,11 @@
+class Pgm4 {
+    public static void main(String[] args) {
+        int time = 20;
+        if (time<18){
+            System.out.println("good morning");
+        else
+        {
+            System.out.println("good evening");
+        }
+}
+}
