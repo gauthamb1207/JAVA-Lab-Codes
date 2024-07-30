96b30250d8515eab05fb2db5db61c77ad5d12579
gauthamb1207 <gautham.balaji2023@vitstudent.ac.in>
2024-07-30 12:31:01 +0530

    Update and rename pgm1.java to Main.java

---
+++ 30-7/Main.java
@@ -0,0 +1,53 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.append("Java");
+        System.out.println(sb);
+    }
+}
+
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.insert(1,"Java");
+        System.out.println(sb);
+    }
+}
+
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.replace(1,3,"Java");
+        System.out.println(sb);
+    }
+}
+
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.delete(1,3);
+        System.out.println(sb);
+    }
+}
+
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
--- 30-7/pgm1.java
+++
@@ -1,9 +0,0 @@
-class Main
-{
-    public static void main(String[] args)
-    {
-        StringBuffer sb = new StringBuffer("Hello ");
-        sb.append("Java");
-        System.out.println(sb);
-    }
-}
